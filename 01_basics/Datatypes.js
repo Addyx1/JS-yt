bee15814617d77287372afe10c60bd9135624834
Addyx1 <rajsinghaditya12345@gmail.com>
2026-01-21 19:07:17 +0000

    container config done

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,8 @@
+let name = "Aditya"
+let age = 22
+let isLoggedIn = false 
+/*null = stand alone value
+symbol = unique */
+console.log(typeof"Aditya");
+console.log(typeof null);
+console.log(typeof undefined);
