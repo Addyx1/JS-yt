1442bc2f32ada2ae91f40184ed18ef0234426f5f
Addyx1 <rajsinghaditya12345@gmail.com>
2026-01-21 19:40:19 +0000

    container config done

---
+++ 01_basics/conversion_operations.js
@@ -0,0 +1,14 @@
+let score = 33
+console.log(typeof score)
+console.log(typeof "score")
+let score1 = "33abc"
+console.log(typeof score1)
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let isloggedIn = 1
+let booleanIsloggedIn = boolean(isloggedIn)
+console.log(isloggedIn)
+let score3 = 33
+let stringNumber = string(score3)
+console.log(typeof stringNumber)
