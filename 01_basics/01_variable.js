5eb765f175f869f4ae4b0878dc7d7d5581c20ba8
Addyx1 <rajsinghaditya12345@gmail.com>
2026-01-21 18:57:14 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "adityaxyz"
+var accountpassword = "12345"
+let accountCity = "phagwara"
+let accountState;
+//console.log(accountId);
+console.log([accountId,accountEmail,accountpassword,accountCity])
+//console.log(accountState);
