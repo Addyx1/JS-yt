1b3a3df444d86d645321174ac033765f5d32aadb
Addyx1 <rajsinghaditya12345@gmail.com>
2026-01-30 20:27:55 +0000

    JSON talk

---
+++ 02_basics/object3.js
@@ -0,0 +1,41 @@
+// de-structure
+
+const course = {
+    coursename : "js in hindi",
+    price: "999",
+    courseInstructor:"Addy"
+
+}
+
+//console.log(course.courseInstructor);
+
+const {courseInstructor:instructor} = course // it will extract courseInstructor from course
+
+//console.log(courseInstructor);
+//console.log(instructor);
+
+
+// const navbar = ({company}) =>{ // curly braces uses to destructure
+
+// }
+
+//navbar(company="Aditya")
+
+// API//
+
+// {
+//     name : "Aditya",
+//     coursename: "JS in hindi",
+//     prcice: "free"
+// }
+
+// JSON WILL STORE IN STRING ALL KEYS AND VALUES OF THE OBJECT IN STRING
+
+[
+    {},
+    {},
+    {}
+]
+
+
+
